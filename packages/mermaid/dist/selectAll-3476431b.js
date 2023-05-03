48e41397919f83ea8051d95e9e0d17b2a3946b00
hamad12a <54325150+hamad12a@users.noreply.github.com>
2023-05-03 14:01:03 +0000

    deleting all 'dist' files from pnpm build

--- packages/mermaid/dist/selectAll-3476431b.js
+++
@@ -1,8 +0,0 @@
-import { S as t, B as n, C as o } from "./commonDb-ee617a3e.js";
-function a(e) {
-  return typeof e == "string" ? new t([document.querySelectorAll(e)], [document.documentElement]) : new t([o(e)], n);
-}
-export {
-  a as s
-};
-//# sourceMappingURL=selectAll-3476431b.js.map
